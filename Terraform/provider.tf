49a01779124761e6e851fd57b7749b4266830525
Aamani Kuntamukkala <aamani344@gmail.com>
2026-03-12 15:49:33 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -18,11 +18,6 @@ provider "azurerm" {
   # Option 1: Use environment variables (recommended)
   # Terraform automatically picks up ARM_SUBSCRIPTION_ID, ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID
 
-  # Option 2: Pass via variables (less secure, but explicit)
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 
   default_tags {
     tags = {

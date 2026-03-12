0c623511faa91a6a7ce83b367e0dcc2996663ac3
Aamani Kuntamukkala <aamani344@gmail.com>
2026-03-12 15:37:05 +0530

    Refactor outputs to use module values in Terraform

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,25 +1,25 @@
 output "resource_group_name" {
   description = "Name of the resource group"
-  value       = azurerm_resource_group.rg.name
+  value       = module.vnet.resource_group_name
 }
 
-output "vnet_id" {
-  description = "ID of the Virtual Network"
-  value       = azurerm_virtual_network.vnet.id
+output "vnet_name" {
+  description = "Name of the Virtual Network"
+  value       = module.vnet.vnet_name
 }
 
-output "subnet_id" {
-  description = "ID of the subnet used by AKS"
-  value       = azurerm_subnet.subnet.id
+output "subnet_ids" {
+  description = "IDs of the subnets created"
+  value       = module.vnet.subnet_ids
 }
 
 output "aks_cluster_name" {
   description = "Name of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.name
+  value       = module.aks.cluster_name
 }
 
 output "aks_kube_config" {
   description = "Kubeconfig for accessing the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.kube_config[0].raw_kube_config
+  value       = module.aks.kube_config
   sensitive   = true
 }

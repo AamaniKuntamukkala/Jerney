cb0a107ab6ff1386c0d308cc8ffa78ca5e16f4a9
Aamani Kuntamukkala <aamani344@gmail.com>
2026-03-12 15:52:27 +0530

    Update outputs to reference Azure resources directly

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,25 +1,25 @@
 output "resource_group_name" {
   description = "Name of the resource group"
-  value       = module.vnet.resource_group_name
+  value       = azurerm_resource_group.rg.name
 }
 
 output "vnet_name" {
   description = "Name of the Virtual Network"
-  value       = module.vnet.vnet_name
+  value       = azurerm_virtual_network.vnet.name
 }
 
 output "subnet_ids" {
   description = "IDs of the subnets created"
-  value       = module.vnet.subnet_ids
+  value       = [for s in azurerm_subnet.subnets : s.id]
 }
 
 output "aks_cluster_name" {
   description = "Name of the AKS cluster"
-  value       = module.aks.cluster_name
+  value       = azurerm_kubernetes_cluster.aks.name
 }
 
 output "aks_kube_config" {
   description = "Kubeconfig for accessing the AKS cluster"
-  value       = module.aks.kube_config
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive   = true
 }

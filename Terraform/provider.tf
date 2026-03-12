5c9f940f33eb981f5388e3412ca8f5d897f31706
Aamani Kuntamukkala <aamani344@gmail.com>
2026-03-12 15:51:13 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -15,15 +15,5 @@ terraform {
 provider "azurerm" {
   features {}
 
-  # Option 1: Use environment variables (recommended)
-  # Terraform automatically picks up ARM_SUBSCRIPTION_ID, ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID
 
-
-  default_tags {
-    tags = {
-      Project     = "Journey"
-      Environment = var.environment
-      ManagedBy   = "Terraform"
-    }
-  }
 }
